2c6f9cb9c07dd6082029e41b9e5a76e462541e4f
Lara Klemme <laraklemme@gmail.com>
2021-09-13 19:36:45 +0200

    created view for index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,24 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+
+  def create
+    @article = Article.new(set_article_params)
+    @article.save
+    redirect_to articles_path
+  end
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  private
+
+  def set_article_params
+    params.require(:article).permit(:title, :content)
+  end
 end
